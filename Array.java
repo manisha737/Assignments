bf37cb3840671c99239b8c92fcebe21b832bfb50
manisha737 <shasenimanisha737@gmail.com>
2021-10-29 07:38:33 +0530

    adding all

---
+++ Array.java
@@ -0,0 +1,24 @@
+package org.testyanta.assignments.designpattern;
+
+public class Array {
+
+	public static void main(String[] args){
+				int arr[]= {1,7,6,9,-5};
+				for(int i=0;i<arr.length-1;i++){
+					for(int j=0;j<arr.length-1;j++) {
+						if(arr[j]>arr[j+1]) {
+							int temp=arr[j];
+							arr[j]=arr[j+1];
+							arr[j+1]=temp;
+						}
+					}
+				}
+				for(int i=0;i<arr.length;i++) {
+		       System.out.print(arr[i]+", ");
+			  }
+			
+		}
+
+	}
+
+
