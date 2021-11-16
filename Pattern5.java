d7dc04adc5d834cf709ccc081e746a3756eb764d
manisha737 <shasenimanisha737@gmail.com>
2021-11-16 17:22:26 +0530

    "adding"

--- Pattern5.java
+++ Pattern5.java
@@ -3,7 +3,6 @@ package org.testyanta.assignments.designpattern;
 public class Pattern5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
         for(int i=0;i<5;i++) {
         	for(int j=0;j<5;j++) {
         		if(i==j || i+j==5-1) {
--- ReversePaln.java
+++ ReversePaln.java
@@ -6,18 +6,22 @@ public class ReversePaln {
 	String str= "my name is manisha";
 	String array[]=str.split(" ");
 	String str1="";
+	String str2="";
+	String s=" ";
+	System.out.println("====reverse the word=====");
 	for(int i=array.length-1;i>=0;i--) {
-		String ar=array[i]+"";
-	    for(int j=ar.length()-1;j>=0;j--) {
-		       str1=str1+ar.charAt(j);
-		       
-		       
-	 }
-
-	    
-}
+		 str1=str1+" "+array[i];
+	}
 	System.out.println(str1);
-	
+	System.out.println("==========reverse the string==========");
+	for(int i=str.length()-1;i>=0;i--) {
+		str2=str2+str.charAt(i);
+	}
+	String s2[]=str2.split(" ");
+	for(int i=s2.length-1;i>=0;i--) {
+		 s=s+" "+s2[i];
+	}
+	System.out.println(s);
 	}
 }
 
